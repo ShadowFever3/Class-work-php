0f18056a783cbb68906cf44cadd1c6eb9a81c8d5
ShadowFever3 <90917040+ShadowFever3@users.noreply.github.com>
2022-03-23 08:16:51 -0400

    Commit with code fo ObjectOriented

--- ObjectOrientedProgramming.php
+++ ObjectOrientedProgramming.php
@@ -2,6 +2,32 @@
 <body>
 
 <?php 
+class Fruit{
+    public $name;
+    private $color;
+
+    function setName($name){
+        $this->name = $name;
+    }
+    function getName(){
+        return $this->name;
+    }
+    function setColor($color){
+        $this->color = $color;
+    }
+    function getColor(){
+        return $this->color;
+    }
+}
+$apple = new Fruit();
+$banana = new Fruit();
+
+$apple->setName('Apple');
+$apple->setColor('Red');
+
+echo $apple->getName() . '<br>';
+echo $apple->getColor() . '<br>';
+
 
 
 ?>
